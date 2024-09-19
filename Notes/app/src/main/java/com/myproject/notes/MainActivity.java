38ffc8d6705dc9088a6e0299cf57cab922c92c3a
Vaibhav Kolte <vaibhavkolte95@gmail.com>
2024-09-19 10:37:42 +0530

    toolbar and fab button

--- Notes/app/src/main/java/com/myproject/notes/MainActivity.java
+++ Notes/app/src/main/java/com/myproject/notes/MainActivity.java
@@ -1,24 +1,30 @@
 package com.myproject.notes;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.myproject.notes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        setOnClickHandler();
+    }
+
+    private void setOnClickHandler() {
+        binding.fabButton.setOnClickListener(view -> {
+            Toast.makeText(this, "Replace your action!", Toast.LENGTH_SHORT).show();
         });
     }
 }
