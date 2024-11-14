26d8e345fc817173c14f3acfb39e115a09f4bc48
Vaibhav Kolte <vaibhavkolte95@gmail.com>
2024-11-14 16:26:35 +0530

    Remove unwanted files

--- udemy/android/MyShoppingListApp/app/src/main/java/com/vkolte/myshoppinglistapp/MainActivity.kt
+++ udemy/android/MyShoppingListApp/app/src/main/java/com/vkolte/myshoppinglistapp/MainActivity.kt
@@ -3,14 +3,9 @@ package com.vkolte.myshoppinglistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.vkolte.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
 
@@ -23,19 +18,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Button(
-                            onClick = { /*TODO*/ },
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
-                        ) {
-                            Text(text = "Add Item")
-                        }
-                    }
+                    ShoppingListApp()
                 }
             }
         }
     }
 }
+
